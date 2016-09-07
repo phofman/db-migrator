3ce66d3d99e8e40b322d10485fbade15a33add56
Paweł Hofman <pawel@codetitans.pl>
2016-09-07 23:06:03 +0200

    Added logging debug info

---
+++ src/CodeTitans.DbMigrator.Core/DebugLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeTitans.DbMigrator.Core
+{
+    public static class DebugLog
+    {
+        public static void Write(Exception ex)
+        {
+            WriteLine("### ### ### ### ### ####");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.GetType().Name);
+            Console.WriteLine(ex.StackTrace);
+            if (ex.InnerException != null)
+            {
+                Write(ex.InnerException);
+            }
+        }
+
+        public static void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
