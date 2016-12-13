fe66536be7916007419c0de3ed7dcb8be70c62ea
Paweł Hofman <pawel@codetitans.pl>
2016-12-13 13:47:55 +0100

    Added new versioning manager
    To actually update version in existing 'settings' table given as dedicated column.

---
+++ src/CodeTitans.DbMigrator.Core/Versioning/ExistingTableVersioning.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodeTitans.DbMigrator.Core.Versioning
+{
+    /// <summary>
+    /// Updates a field inside existing table to store information about database version.
+    /// </summary>
+    public class ExistingTableVersioning : IDbVersionManager
+    {
+        private readonly string _tableName;
+        private readonly string _columnName;
+
+        /// <summary>
+        /// Init constructor.
+        /// </summary>
+        public ExistingTableVersioning(string tableName, string columnName)
+        {
+            if (string.IsNullOrEmpty(tableName))
+                throw new ArgumentNullException(nameof(tableName));
+            if (string.IsNullOrEmpty(columnName))
+                throw new ArgumentNullException(nameof(columnName));
+
+            _tableName = tableName;
+            _columnName = columnName;
+        }
+
+        /// <inheritdoc />
+        public async Task<Version> GetVersionAsync(IDbExecutor executor)
+        {
+            var exists = await executor.CheckIfTableExistsAsync(_tableName);
+            if (!exists)
+                return null;
+
+            try
+            {
+                var query = $"SELECT [{_columnName}] AS Version FROM [{_tableName}]";
+                var versionString = await executor.ExecuteScalarAsync<string>(query);
+
+                return new Version(versionString);
+            }
+            catch (Exception ex)
+            {
+                DebugLog.Write(ex);
+                return null;
+            }
+        }
+
+        /// <inheritdoc />
+        public Task<bool> UpdateAsync(IDbExecutor executor, MigrationScript script, int scriptBatchIndex, IEnumerable<ScriptParam> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task OnSkippedAsync(IDbExecutor executor, MigrationScript script, int scriptBatchIndex, IEnumerable<ScriptParam> args)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ tests/UnitTests/VersioningTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeTitans.DbMigrator.Core.Migrations.TSql;
+using CodeTitans.DbMigrator.Core.Versioning;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public sealed class VersioningTests
+    {
+        [TestMethod]
+        public void GetVersionOfDatabase_with_success()
+        {
+            var executor = new DbTSqlWorker("(localdb)\\thb", "THBDB", null, null);
+            var manager = new ExistingTableVersioning("KonfiguracjaSystemu", "WersjaDB");
+
+            var version = executor.GetVersionAsync(manager).Result;
+            Assert.IsNotNull(version);
+        }
+    }
+}
